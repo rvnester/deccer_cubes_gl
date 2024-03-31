eab59786393ef139185ad3fc87bbd3fa0ff86ad4
Legion\Whatever <Whatever@gmail.com>
2024-04-01 01:06:11 +0300

    Fixed an error, supplied wrong object into sampler functions.

--- main.cpp
+++ main.cpp
@@ -444,15 +444,17 @@ int main(void)
     GLuint sampler;
     glCreateSamplers(1, &sampler);
 
-    glSamplerParameteri(texture, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glSamplerParameteri(texture, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glSamplerParameteri(sampler, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glSamplerParameteri(sampler, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glSamplerParameteri(texture, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glSamplerParameteri(texture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glSamplerParameteri(texture, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
     // Don't forget to bind the sampler to a texture unit
     glBindSampler(0, sampler);
+
+
 
     // Vertex Stage
     const GLchar* vertexShaderSource[] = { shader_vs.c_str() };
